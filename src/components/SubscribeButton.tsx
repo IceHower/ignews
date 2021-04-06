b495d2fa22635b5cb1f89174092498c1353f4a37
Vinicius Telles <icehower@Viniciuss-MacBook-Air.local>
2021-04-06 00:41:16 -0300

    Adding getStaticProps

--- src/components/SubscribeButton.tsx
+++ src/components/SubscribeButton.tsx
@@ -1,6 +1,10 @@
 import styles from '../styles/components/subscribebutton.module.scss';
 
-export function SubscribeButton() {
+
+interface SubscribreButtonProps {
+    priceId: string,
+}
+export function SubscribeButton({ priceId } : SubscribreButtonProps) {
     return (
         <button type="button" className={styles.subscribeButton}>
             Subscribe Now
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,18 @@ import { AppBar } from '../components/AppBar';
 import styles from '../styles/pages/home.module.scss';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { GetStaticProps } from 'next';
+import { stripe } from '../services/stripe';
 
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number
+  }
+}
 
-
-export default function Home() {
+export default function Home({product} : HomeProps) {
+  console.log(product.amount)
   return (
     <>
       <Head>
@@ -18,12 +26,33 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br/> 
-            <span> for $9.90 month</span>  
+            <span> for {product.amount} month</span>  
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/assets/avatar.svg" alt="Girl coding"/>
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async() => {
+      const price = await stripe.prices.retrieve('price_1Id2OoLW6fMV8xFFfiqry1Hz', {
+        expand: ['product']
+      }); // Faz a chamada da api
+
+      const product = {
+        priceId: price.id,
+        amount: new Intl.NumberFormat('en-US', {
+          style: 'currency',
+          currency: 'USD',
+        }).format((price.unit_amount / 100)),
+
+      }
+      return {
+        props: {
+          product,
+        },
+        revalidate: 60 * 60 * 24 // 24h - Quanto tempo em segundos a pagina precisa ser revalidada
+      }
 }
