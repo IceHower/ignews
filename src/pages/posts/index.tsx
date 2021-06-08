1153aceb3b007016fbb8381351c3f415fc7d42ba
IceHower <vctelles09@gmail.com>
2021-06-08 16:24:57 -0300

    add a full page post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss'
 import Prismic from '@prismicio/client';
+import Link from 'next/link'
 import { RichText } from 'prismic-dom';
 
 type Post = {
@@ -26,12 +27,14 @@ export default function Posts({ posts }: PostsProps) {
 
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    {posts.map( post => (
-                         <a key={post.slug} href="#">
-                         <time>{post.updatedAt}</time>
-                         <strong>{post.title}</strong>
-                         <p>{post.excerpt}</p>
-                     </a>
+                    {posts.map(post => (
+                        <Link href={`/posts/${post.slug}`}>
+                            <a key={post.slug}>
+                                <time>{post.updatedAt}</time>
+                                <strong>{post.title}</strong>
+                                <p>{post.excerpt}</p> 
+                            </a>
+                        </Link>
                     ))}
                 </div>
             </main>
